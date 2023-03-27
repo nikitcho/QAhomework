a0a601d76173522fea9b56614f3a3f27dc72028a
nikitcho <niki_73b@abv.bg>
2023-03-27 16:54:11 +0300

    a

--- src/main/java/QA/HomeWork.java
+++ src/main/java/QA/HomeWork.java
@@ -1,4 +1,19 @@
 package QA;
 
+import java.util.Scanner;
+
 public class HomeWork {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String userName = scanner.nextLine();
+        System.out.println("Nice to meet you," + userName);
+
+        System.out.println("What is your hobby," + userName);
+        String hobby = scanner.nextLine();
+        System.out.println( hobby + " is a very nice hobby of yours," + userName);
+
+    }
 }
